5367ab2204b86b4d29463056a5f41e58342dfc3f
rivailJunior <rivail.rj@gmail.com>
2024-02-25 23:39:52 -0400

    fix: room and hotel, mocking tests

--- src/tests/unit/hotel.test.ts
+++ src/tests/unit/hotel.test.ts
@@ -1,5 +1,5 @@
-import { connection, disconnect } from "@/infra/data-base";
-import { describe, test, expect } from "vitest";
+import connection, { disconnect } from "@/infra/data-base";
+import { describe, test, expect, vi } from "vitest";
 
 afterEach(() => {
   (async () => {
@@ -10,6 +10,10 @@ afterEach(() => {
 //hotel
 describe("Hotel", () => {
   test("should read one hotel", async () => {
+    vi.spyOn(connection.hotel, "findFirst").mockResolvedValue({
+      id: 1,
+      name: "Hotel Brazil",
+    });
     const hotel = await connection.hotel.findFirst();
     expect(hotel).toStrictEqual({
       id: 1,
--- src/tests/unit/rooms.test.ts
+++ src/tests/unit/rooms.test.ts
@@ -1,6 +1,6 @@
-import { connection, disconnect } from "@/infra/data-base";
+import connection, { disconnect } from "@/infra/data-base";
 import { Prisma } from "@prisma/client";
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, vi } from "vitest";
 
 afterEach(() => {
   (async () => {
@@ -8,16 +8,26 @@ afterEach(() => {
   })();
 });
 
+const resolvedValue = {
+  id: 1,
+  hotelId: 1,
+  description: "A1",
+  pictures: "/public/rooms/hotel-brazil",
+  dayPrice: new Prisma.Decimal(100),
+};
 //rooms
 describe("Rooms", () => {
   test("should read the first room of the hotel brazil", async () => {
+    vi.spyOn(connection.hotelRoom, "findFirst").mockResolvedValueOnce(
+      resolvedValue
+    );
     const hotelRoom = await connection.hotelRoom.findFirst();
     expect(hotelRoom).toStrictEqual({
       id: 1,
       hotelId: 1,
       description: "A1",
       pictures: "/public/rooms/hotel-brazil",
-      dayPrice: new Prisma.Decimal(hotelRoom?.dayPrice || 0),
+      dayPrice: new Prisma.Decimal(100),
     });
   });
 });
